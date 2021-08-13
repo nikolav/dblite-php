2731241f794f0db3f54906ce719e8c29f2626544
nikolav <vukovicnikola2014@gmail.com>
2021-08-13 11:00:09 +0100

    updates licence, deletes public

--- index.php
+++ index.php
@@ -13,10 +13,11 @@ $bp->maindb = dblite::start('./main.db');
 
 $bp->active('maindb');
 
+
 $q = <<<EOQ_
 select count(*) as tot from main
 EOQ_;
-
+// $bp->db->exec($q);
 
 printf("[# of records]: %s", $bp->db->q($q));
 
